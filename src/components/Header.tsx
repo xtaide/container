497cc49c6613da56a75640ff9d10c65b179c5cab
Danila Ivanov <xtaide@icloud.com>
2023-07-27 23:36:15 +0300

    #0 init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import './Header.css'
 export const Header:React.FC = () => {
     const {count} = useStore()
     return (
-        <div className='header'>Counter: {count}</div>
+        <div className='header'>Counter: {count }</div>
     )
 }
 
